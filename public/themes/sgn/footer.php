98e9267aa73a7ddb0b8c19bbdd38d04c53b0735a
Isa Areschoug <isaar@Isas-MacBook-Air.local>
2019-04-25 12:03:57 +0200

    Dividing CSS and cleaning up markup

--- public/themes/sgn/footer.php
+++ public/themes/sgn/footer.php
@@ -8,37 +8,7 @@
 ?>
      </div>
   </div>
-  <!-- <footer>
-    <div class="footer-container">
-      <div class="footer-logo">
-
-      </div>
-      <div class="footer-info">
-        <p>Information</p>
-        <ul>
-          <li>About us</li>
-          <li>Activities</li>
-          <li>Work</li>
-          <li>Contact</li>
-          <li>Participate</li>
-        </ul>
-      </div>
-      <div class="footer-adress">
-        <p>Visit us</p>
-        <p>Doktor Sellbergs väg 5, <br>
-          Restad Gård,<br>
-          462 54 Vänersborg
-        </p>
-      </div>
-      <div class="footer-contact">
-        <p>Contact us</p>
-        <ul>
-          <li>+46 768 84 08 84</li>
-          <li>info@supportgroup.se</li>
-        </ul>
-      </div>
-    </div>
-  </footer> -->
+  
 <?php wp_footer(); ?>
 
 </body>
